0e09e99b0ab6661a2135fcdf817d0f3e1420d994
lucaramonsmith <lucaramonsp@gmail.com>
2026-05-26 12:10:26 -0300

    base

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,15 @@ let totalTexto: string = preguntar('Total de la cuenta: ');
 let esEstudiante: string = preguntar('¿Son estudiantes de ORT? si/no: ');
 let personasTexto: string = preguntar('Dividir la cuenta entre: ');
 
-let totalPorPersona: number = 0; // COMPLETAR
+let totalPorPersona: number = 0; 
+let cuenta: number = parseFloat (totalTexto)
+let personas: number = parseFloat (personasTexto)
+
+if (esEstudiante === "si") {
+    totalPorPersona = cuenta * 0.90 / personas 
+}
+
+else {
+    totalPorPersona = cuenta * 0.90
+}
 console.log(`Total por persona: $${totalPorPersona}`);
